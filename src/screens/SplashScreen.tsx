ca92c198ffc9e89cf03488f04fb8bf21f7a161a7
nikhil7174 <nikhilkumarsingh7174@gmail.com>
2023-11-24 20:19:07 +0530

    created splash screen and welcome screen

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,12 +1,46 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+//@ts-nocheck
+import { View, Text } from "react-native";
+import React, { useEffect } from "react";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { useNavigation } from "@react-navigation/native";
 
-const SplashScreen = () => {
+export default function SplashScreens() {
+  const navigation = useNavigation();
+
+  const [fontsLoaded, fontError] = useFonts({
+    SpaceGroteskSemiBold: require("../fonts/SpaceGrotesk-SemiBold.ttf"),
+    SpaceGroteskBold: require("../fonts/SpaceGrotesk-Bold.ttf"),
+    SpaceGroteskMedium: require("../fonts/SpaceGrotesk-Medium.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+
+    setTimeout(() => {
+      //@ts-ignore
+      navigation.navigate("Welcome"); // Navigate to HomeTab
+    }, 3000); // 3 seconds delay
+  });
+
+  useEffect(() => {
+    onLayoutRootView();
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View>
-      <Text>SplashScreen</Text>
+    <View
+      onLayout={onLayoutRootView}
+      className="flex-1 bg-orange-800 justify-center items-center"
+    >
+      <Text className="text-white text-3xl font-extrabold uppercase">
+        Stacks
+      </Text>
     </View>
-  )
+  );
 }
-
-export default SplashScreen
